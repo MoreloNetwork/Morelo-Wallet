f9ad141725699f98f9e591941982345136bbc39f
mssc89 <oweziz89@gmail.com>
2021-02-17 13:43:12 +0100

    Added login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./pages/welcome/main/main.module').then( m => m.MainPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/welcome/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/welcome/register/register.module').then( m => m.RegisterPageModule)
   }
 ];
 @NgModule({
