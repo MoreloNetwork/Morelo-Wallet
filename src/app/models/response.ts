759a1c6753527e865b4c9bb5f314f5ef73565bc6
mssc89 <oweziz89@gmail.com>
2021-02-17 13:51:57 +0100

    User and response models fill

--- src/app/models/response.ts
+++ src/app/models/response.ts
@@ -1,2 +1,4 @@
 export class Response {
+  status: any;
+  data: any;
 }
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,2 +1,9 @@
 export class User {
+  constructor(
+      public id: number = 0,
+      public login: string = '',
+      public password: string = '',
+      public email: string = '',
+      public username: string = ''
+  ) {}
 }
