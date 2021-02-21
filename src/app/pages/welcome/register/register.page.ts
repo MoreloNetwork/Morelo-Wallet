b5d621366a5154ce68c62f108183071ab70f2a2b
mssc89 <oweziz89@gmail.com>
2021-02-21 12:09:32 +0100

    Register form with validation

--- src/app/pages/welcome/register/register.page.ts
+++ src/app/pages/welcome/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  isSubmitted = false;
+
+  constructor(private auth: AuthService, public formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      login: ['', [Validators.required, Validators.minLength(4)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
+    })
+  }
 
   ngOnInit() {
+  }
+
+  get errorControl() {
+    return this.registerForm.controls;
+  }
+
+  submitForm() {
+    this.isSubmitted = true;
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value)
+    }
   }
 
 }
